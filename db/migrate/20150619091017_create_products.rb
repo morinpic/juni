bd47d0be445b46cd6274405dc532114949c900c9
morinpic <morinpic0518@gmail.com>
2015-06-19 18:30:47 +0900

    - update routing

--- db/migrate/20150619091017_create_products.rb
+++ db/migrate/20150619091017_create_products.rb
@@ -1,8 +1,8 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.string :title
-      t.text :body
+      t.string :title, null:false
+      t.text :body, null:false
 
       t.timestamps null: false
     end
