e760001c805d2c49c1d8e841634050312f0c9160
morinpic <morinpic0518@gmail.com>
2015-06-20 03:52:51 +0900

    - fix bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def hero_path(p)
     if p.present?
       # ランダムで画像パスを取得
-      p[rand(p.count)+1].pictures[0].image.url(:l)
+      p[rand(p.count)].pictures[0].image.url(:l)
     end
   end
 end
