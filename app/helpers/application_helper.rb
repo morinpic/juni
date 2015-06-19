de0ad8f4f057aa31c64dbf6daccf14b3437fdbef
morinpic <morinpic0518@gmail.com>
2015-06-20 03:46:33 +0900

    - edit hero画像をランデムで取得するように修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
   def hero_path(p)
     if p.present?
-      p[0].pictures[0].image.url(:l)
+      # ランダムで画像パスを取得
+      p[rand(p.count)+1].pictures[0].image.url(:l)
     end
   end
 end
