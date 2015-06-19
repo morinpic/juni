3ca0bc3b07a13c8a51b92e563814c053fbe265ba
morinpic <morinpic0518@gmail.com>
2015-06-19 19:21:03 +0900

    - edit product/index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,11 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:tilte, :body)
   end
 end
