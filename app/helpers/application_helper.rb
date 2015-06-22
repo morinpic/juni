33273f70d5798dfb5b873edb9c090989f8ab1770
morinpic <morinpic0518@gmail.com>
2015-06-22 15:13:37 +0900

    - add ProductモデルにCategoryカラム追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,8 @@ module ApplicationHelper
   def hero_path(p)
     if p.present?
       # ランダムで画像パスを取得
-      p[rand(p.count)].pictures[0].image.url(:l)
+      num = rand(p.count)
+      p[num].pictures[0].present? ? p[num].pictures[0].image.url(:l) : ''
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619103729) do
+ActiveRecord::Schema.define(version: 20150622060418) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20150619103729) do
     t.text     "body",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "category",   null: false
   end
 
 end
